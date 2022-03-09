425c75f0efaed064d93dca06a2fb402db1d94479
Farhang Alizadeh <farhang.alizadeh103@gmail.com>
2022-03-09 22:54:42 +0330

    initial

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen bg-black text-white flex justify-center items-center text-9xl">
+      FJ
+    </div>
+  );
+}
+
+export default App;
